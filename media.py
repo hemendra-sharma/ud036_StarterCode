dc4f69fb8ae7a07ec25cd05aecf2459a78d571ba
hemendra-sharma <32269609+hemendra-sharma@users.noreply.github.com>
2017-09-25 23:09:49 +0530

    Code style fixes after review.

--- media.py
+++ media.py
@@ -1,22 +1,24 @@
-"""This package contains the Movie class that holds all the movie-related data 
+"""This package contains the Movie class that holds all the movie-related data
 that will be shown on movie trailer website."""
 
+
 class Movie():
-	"""This class contains all the information related to the movie.
-		i.e. This class is used to display the movie title and poster.
-		This class also contains the youtube trailer URL which can be 
-		used to play the trailer-video when clicked on poster."""
+    """This class contains all the information related to the movie.
+    i.e. This class is used to display the movie title and poster.
+    This class also contains the youtube trailer URL which can be
+    used to play the trailer-video when clicked on poster."""
 
 
-	def __init__(self, title, storyline, poster_image_url, 
-					trailer_youtube_url):
-		self.title = title # is used for showing the 
-							# movie title below poster image
-		self.storyline = storyline # not being used right now,
-									# but later can be used to display
-									# story line when mouse hovering over
-									# movie poster
-		self.poster_image_url = poster_image_url
-								# is used to show movie poster
-		self.trailer_youtube_url = trailer_youtube_url
-								# is used to play the movie trailer in a pop up box
+    def __init__(self, title, storyline, poster_image_url,
+    	trailer_youtube_url):
+        self.title = title  # is used for showing the
+                            # movie title below poster image
+        self.storyline = storyline  # not being used right now,
+                                    # but later can be used to display
+                                    # story line when mouse hovering over
+                                    # movie poster
+        self.poster_image_url = poster_image_url
+                                # is used to show movie poster
+        self.trailer_youtube_url = trailer_youtube_url
+                                # is used to play the movie trailer
+                                # in a pop up box
